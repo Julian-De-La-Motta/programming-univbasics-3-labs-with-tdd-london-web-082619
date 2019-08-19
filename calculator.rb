bdab81b55846091fe902155299d28ad567436972
glorious-communicator-0403 <juliandelamotta@gmail.com>
2019-08-19 14:34:56 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,5 @@
-first_number ="hello"
-second_number ==2 
+first_number =2
+second_number =4 
 sum = first_number+second_number
 difference=first_number-second_number
 product=first_number*second_number
